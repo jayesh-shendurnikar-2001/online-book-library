78489c09e2eba635a92cd76edc9abee1e7572410
jayeshshendurnikar <shendurnikarjayesh2001@gmail.com>
2026-02-09 15:14:46 +0530

    added book add logic

---
+++ src/pages/AddBooks.jsx
@@ -0,0 +1,110 @@
+import { useDispatch } from "react-redux";
+import { addBook } from "../features/booksSlice";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+
+const DEFAULT_IMAGE =
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3JKqrQRlKbSAGPyewnePY0TmNzWTeNTyYcg&s";
+
+export default function AddBook() {
+  const [form, setForm] = useState({});
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!form.title || !form.author || !form.category || !form.rating) {
+      alert("All fields required");
+      return;
+    }
+
+    if (form.rating < 1 || form.rating > 5) {
+      alert("Rating must be between 1 and 5");
+      return;
+    }
+
+    dispatch(
+      addBook({
+        ...form,
+        id: Date.now(),
+        url: form.url || DEFAULT_IMAGE,
+        rating: Number(form.rating),
+      })
+    );
+
+    navigate("/books");
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className="p-6 space-y-3">
+
+      <label>Book Title <span className="text-red-600">*</span></label>
+      <input
+        required
+        placeholder="Title"
+        className="border p-2 w-full"
+        onChange={(e) => setForm({ ...form, title: e.target.value })}
+      />
+
+      <label>Author Name <span className="text-red-600">*</span></label>
+      <input
+        required
+        placeholder="Author"
+        className="border p-2 w-full"
+        onChange={(e) => setForm({ ...form, author: e.target.value })}
+      />
+
+      <label>Select Category <span className="text-red-600">*</span></label>
+      <select
+        required
+        className="border p-2 w-full"
+        defaultValue=""
+        onChange={(e) => setForm({ ...form, category: e.target.value })}
+      >
+        <option value="" disabled>
+          Select Category
+        </option>
+        <option value="Fiction">Fiction</option>
+        <option value="Non-Fiction">Non-Fiction</option>
+        <option value="Sci-Fi">Sci-Fi</option>
+        <option value="Adventure">Adventure</option>
+        <option value="Coding">Coding</option>
+      </select>
+
+      <label>Rating <span className="text-red-600">*</span></label>
+      <input
+        required
+        type="float"
+        placeholder="Rating (1–5)"
+        className="border p-2 w-full"
+        onChange={(e) => setForm({ ...form, rating: Number(e.target.value) })}
+      />
+
+      <label>Img Url (Optional)</label>
+      <input
+        placeholder="Image URL (optional)"
+        className="border p-2 w-full"
+        onChange={(e) => setForm({ ...form, url: e.target.value })}
+      />
+
+      <label>Description</label>
+      <textarea
+        placeholder="Description"
+        className="border p-2 w-full"
+        onChange={(e) => setForm({ ...form, description: e.target.value })}
+      />
+
+      <button
+        className="
+        bg-green-500 text-white px-5 py-2 rounded-lg
+        shadow-[0_8px_24px_oklch(0_0_0_/_0.16)]
+        transition-all duration-300 ease-out
+        hover:bg-green-600 hover:-translate-y-1 hover:shadow-[0_12px_32px_oklch(0_0_0_/_0.18)]
+        active:translate-y-0 active:scale-95"
+      >
+        Add Book
+      </button>
+    </form>
+  );
+}
